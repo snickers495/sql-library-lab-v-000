dfa843b74f6f77f379fa1efd9a2685defad65418
Lisa Lee <snickers495@gmail.com>
2017-10-19 20:47:48 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors INNER JOIN series ON authors.series_id = series.id JOIN subgenres ON
+  "SELECT authors.name, subgenres.name FROM authors INNER JOIN series ON authors.serie_id = series.id JOIN subgenres ON
   series.subgenre_id = subgenres.id; "
 end
 
